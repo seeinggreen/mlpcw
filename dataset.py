86798be1bc4a55a256e14779571f6ba48a9e7e93
Dan Green <dan.green@hw.ac.uk>
2023-03-02 14:50:01 +0000

    Added comments and dataset stats

--- dataset.py
+++ dataset.py
@@ -3,47 +3,145 @@ import os
     
 class Dataset:
     
-    def __init__(self,load_data=False):
+    def __init__(self,load_data=False,exclude_ids=True):
+        """
+        Creates a Dataset object and (optionally) loads the data from file
+
+        Parameters
+        ----------
+        load_data : bool, optional
+            If True, the dataset is loaded from file. The default is False.
+            
+        exclude_ids : bool, optional
+            If True, all_data will exclude the TMDb/Wikipedia IDs. The default is True.
+
+        Returns
+        -------
+        None.
+
+        """
         if load_data:
-            self.load_dataset()
-    
-    def load_dataset(self):
+            self.load_dataset(exclude_ids)
+            
+    def print_stats(self):
+        """
+        Calculates and prints some basic stats about the dataset loaded in memory.
+
+        Returns
+        -------
+        None.
+
+        """
+        total_entries = len(self.all_data)
+        wiki_plot_word_lengths = [len(film['wiki_plot'].split(' ')) for film in self.all_data]
+        tmdb_plots_word_lengths = [len(film['tmdb_plot'].split(' ')) for film in self.all_data]
+        mean_wiki_plot_length = sum(wiki_plot_word_lengths) / total_entries
+        mean_tmdb_plot_length = sum(tmdb_plots_word_lengths) / total_entries
+        ratios = [wiki_plot_word_lengths[i] / tmdb_plots_word_lengths[i] for i in range(total_entries)]
+        mean_ratio = sum(ratios) / total_entries
+        shorter = len([ratio for ratio in ratios if ratio < 1])
+        ratio2 = len([ratio for ratio in ratios if ratio >= 1 and ratio < 2])
+        ratio3 = len([ratio for ratio in ratios if ratio >= 2 and ratio < 3])
+        ratio5 = len([ratio for ratio in ratios if ratio >= 3 and ratio < 5])
+        ratio7 = len([ratio for ratio in ratios if ratio >= 5 and ratio < 7])
+        longer = len([ratio for ratio in ratios if ratio >= 7])
+        
+        print(f'Total entries in dataset: {total_entries}')
+        print(f'Mean length of Wikipedia plots: {mean_wiki_plot_length:.1f} words')
+        print(f'Mean length of TMDb plots: {mean_tmdb_plot_length:.1f} words')
+        print(f'Mean ratio of Wikipedia:TMDb plot lengths: {mean_ratio:.2f}')
+        print(f'Films with shorter Wikipedia plots: {shorter} ({shorter/total_entries*100:.2f}% of entries)')
+        print(f'Ratios between 1-2: {ratio2} ({ratio2/total_entries*100:.2f}% of entries)')
+        print(f'Ratios between 2-3: {ratio3} ({ratio3/total_entries*100:.2f}% of entries)')
+        print(f'Ratios between 3-5: {ratio5} ({ratio5/total_entries*100:.2f}% of entries)')
+        print(f'Ratios between 5-7: {ratio7} ({ratio7/total_entries*100:.2f}% of entries)')
+        print(f'Ratios greater than 7: {longer} ({longer/total_entries*100:.2f}% of entries)')
+        
+    def load_dataset(self,exclude_ids):
+        """
+        Loads the data from file and stores it in a dictionary (data_by_year) and a list (all_data).
+        
+        Parameters
+        ----------
+        exclude_ids : bool, optional
+            If True, all_data will exclude the TMDb/Wikipedia IDs. The default is True.
+
+        Returns
+        -------
+        None.
+
+        """
         self.data_by_year = {}
+        self.all_data = []
         for file in os.listdir('dataset_jsons'):
             year = file[:4]
             with open(os.path.join('dataset_jsons',file)) as f:
                 self.data_by_year[year] = json.load(f)
+            self.all_data.extend(self.data_by_year[year])
+          
+        #Optionally remove the IDs from each film entry
+        if exclude_ids:
+            for film in self.all_data:
+                film.pop('tmdb_id',None)
+                film.pop('wiki_id',None)
     
     def build_and_store_datset(self):
+        """
+        Rebuilds the dataset and stores it to file.
+
+        Returns
+        -------
+        None.
+
+        """
         self.build_dataset()
         self.store_dataset()
     
     def store_dataset(self):
+        """
+        Stores the dataset held in memory to file.
+
+        Returns
+        -------
+        None.
+
+        """
         self.build_dataset()
         for year in self.data_by_year:
             with open(f'dataset_jsons/{year}.json','w') as file:
                 json.dump(self.data_by_year[year],file)
     
     def build_dataset(self):
+        """
+        Builds the dataset from TMDb and Wikipedia data.
+
+        Returns
+        -------
+        None.
+
+        """
+        #Get the Wikipedia data
         wiki_data = {}
         for file in os.listdir('wiki_jsons'):
             with open(os.path.join('wiki_jsons',file)) as f:
                 year = json.load(f)
                 for page in year:
                     wiki_data[page] = year[page]
-            
+        
+        #Get the TMDb data
         tmdb_data = {}
         for file in os.listdir('tmdb_jsons'):
             with open(os.path.join('tmdb_jsons',file)) as f:
                 year = json.load(f)
                 for film in year:
-                    year[film]['year'] = file[:4]
+                    year[film]['year'] = file[:4] #Preserve the TMDb release year for later access
                     tmdb_data[film] = year[film]
         
+        #Filter to just the Wikipedia pages that have valid TMDb IDs attached
         tm_wiki_films = [page_id for page_id in wiki_data if 'tmdb_id' in wiki_data[page_id]]
         
         self.data_by_year = {}
-        missing = []
+        missing = [] #Keep track of missing TMDb entries
         for wiki_id in tm_wiki_films:
             tmdb_id = str(wiki_data[wiki_id]['tmdb_id'])
             if tmdb_id not in tmdb_data:
@@ -53,6 +151,7 @@ class Dataset:
             title = tmdb['title']
             year = tmdb['year']
             genres = tmdb['genres']
+            #Reject entries with no genres stored in TMDb
             if not genres:
                 continue
             tmdb_plot = tmdb['plot']
--- wiki.py
+++ wiki.py
@@ -24,33 +24,54 @@ class Wiki:
 
         """
         self.wiki = wikipediaapi.Wikipedia('en')
+        #Start an API session for manual API calls
         self.session = requests.Session()
         
     def fetch_and_store_plots(self,tmdb):
+        """
+        Fetches the plot from Wikipedia movie pages.
+
+        Parameters
+        ----------
+        tmdb : Tmdb
+            A Tmdb object for translating IMDb IDs to TMDb IDs.
+
+        Returns
+        -------
+        None.
+
+        """
+        #The total number of Wikipedia pages to process
         total = sum([len(self.wiki_pages[year]['pages']) for year in self.wiki_pages])
+        #A list of years that are already (partially) processed
         done = [f[:4] for f in os.listdir('wiki_jsons')]
+        #Keep track of pages that didn't complete sucessfully
         self.errors = {}
 
         with tqdm(total=total) as pbar:
             for i,year in enumerate(self.wiki_pages):
+                #If the year has been processed, get the pages already on file
                 raw_films = {}
                 if str(year) in done:
                     with open(f'wiki_jsons/{year}.json') as f:
                         raw_films = json.load(f)
                 print(f'Fetching year {i+1} of {len(self.wiki_pages)} ({year}) - {pbar.n}/{pbar.total}:')
-                self.errors=[]
+                self.errors[year]=[]
                 try:
                     pages = self.wiki_pages[year]['pages']
                     films = {}
                     titles = []
+                    #For existing entries, keep them if there was no error
                     for film in raw_films:
                         if not 'error' in raw_films[film]:
                             films[film] = raw_films[film]
                             titles.append(films[film]['title'])
-                    pbar.update(len(pages))
+                    #If there were no errors, update the progress bar and move on
                     if len(films) == len(raw_films):
+                        pbar.update(len(pages))
                         continue
                     for page in pages:
+                        #Titles are preloaded, so no API call is required if the page gets skipped
                         title = page.title
                         if title in titles:
                             continue
@@ -59,6 +80,7 @@ class Wiki:
                             plot = self.get_plot(page)
                             if plot:
                                 films[page_id] = {'title':title,'plot':plot}
+                                #Use the IMDb ID (if present) to fetch a TMDb ID
                                 imdb_id = self.get_imdb_id(page_id)
                                 if imdb_id:
                                     tmdb_id = tmdb.get_tmdb_id_from_imdb_id(imdb_id)
@@ -70,26 +92,56 @@ class Wiki:
                             return
                         except:
                             films[page_id] = {'title':title,'error':True}
-                            self.errors.append(page_id)
+                            self.errors[year].append(page_id)
                             print(f'Error with page #{page_id} ({title}) in {year}')
                         pbar.update(1)
-                    if films:
+                    if films: #Don't create empty JSON files
                         with open(f'wiki_jsons/{year}.json','w') as file:
                             json.dump(films,file)
                 except(Exception) as err:
                     print(f'Error with {year}: {err}')
         
     def get_imdb_id(self,page_id):
+        """
+        Scrapes the external links on a Wikipedia page to try and get an IMDb ID.
+
+        Parameters
+        ----------
+        page_id : int or str
+            The ID of the Wikipedia page.
+
+        Returns
+        -------
+        str or None
+            Returns the IMDb ID if present, otherwise returns None.
+
+        """
         links = self.get_ext_links(page_id)
+        #Regex matches main pages of films and captures the ID in group 1
         imdb_links = [re.match(IMDB_REGEX,link).group(1) for link in links if re.match(IMDB_REGEX,link)]
         if len(imdb_links) == 0:
             return None
+        #Reject pages that have multiple IMDb films linked
         if len(imdb_links) > 1:
             return None
         else:
             return imdb_links[0]
         
     def get_ext_links(self,page_id):
+        """
+        Performs a manual API call to fetch the external links on a Wikipedia page.
+
+        Parameters
+        ----------
+        page_id : int or str
+            The ID of the Wikipedia page.
+
+        Returns
+        -------
+        links : list of str
+            The external links on the given Wikipedia page.
+
+        """
         params = {
             "action": "parse",
             "pageid": page_id,
@@ -102,50 +154,104 @@ class Wiki:
         return links
     
     def fetch_all_years(self,years_to_fetch):
+        """
+        Fetches the Wikipedia pages about films using categories for the given list of years.
+
+        Parameters
+        ----------
+        years_to_fetch : iterable
+            The years to check categories for.
+
+        Returns
+        -------
+        None.
+
+        """
         self.wiki_pages = {}
-        
-        years = []
+        years = [] #Will be filled with years with valid category pages
         
         print('Fetching main year pages...')
         for year in tqdm(years_to_fetch):
             pages,sub_cats = self.fetch_films_from_year(year)
-            if pages:
+            if pages: #Pages will be None if the category page doesn't exist
                 years.append(year)
                 self.wiki_pages[year] = {}
+                #Store the pages on the main category page and the sub-categories separately
                 self.wiki_pages[year]['pages'] = pages
                 self.wiki_pages[year]['sub_cats'] = sub_cats
             
+        #Get a list of years for which there are sub-categories
         years = [year for year in years if self.wiki_pages[year]['sub_cats']]
         level = 0
         
         while(years):
+            #Recursively fetch sub-categories until there are no more years with further sub-cats
             print(f"Fetching {'sub '*(level+1)}category pages for {len(years)} years...")
             years = self.get_sub_cat_pages(years, level)
             level += 1
             
         for year in tqdm(years):
+            #Keep a set of all IDs already listed for the given year
             ids = [page.pageid for page in self.wiki_pages[year]['pages']]
             self.wiki_pages[year]['id_set'] = set(ids)
             
+            #Add the sub-category pages for each level of sub-category
             for l in range(level):
                 self.add_sub_cat_pages(year, l)
     
     def add_sub_cat_pages(self,year,level):
+        """
+        Attempts to add a level of sub-category pages to the main list of pages for a given year.
+
+        Parameters
+        ----------
+        year : int
+            The year the pages relate to.
+        level : int
+            The level below the main year's page.
+
+        Returns
+        -------
+        None.
+
+        """
+        #Each layer's pages to add will be in their own list
         ps = 'extra_'*(level+1) + 'pages'
         if year not in self.wiki_pages[year]:
-            return
+            return #Return if there are no extra pages to add
         pages = self.wiki_pages[year][ps]
         
+        #For each extra page, add it if the ID isn't already present
         for page in pages:
             page_id = page.pageid
             if page_id not in self.wiki_pages[year]['id_set']:
                 self.wiki_pages[year]['pages'].append(page)
+                #Update the set of page IDs to reflect the addition
                 self.wiki_pages[year]['id_set'].add(page_id)
         
     def get_sub_cat_pages(self,years,level):
+        """
+        Fetches the pages listed in a given sub-category.
+
+        Parameters
+        ----------
+        years : list of int
+            The years to fetch sub-categories for.
+        level : int
+            The level below the main year's page.
+
+        Returns
+        -------
+        next_level_years : list of int
+            The years which have sub-categories at the next level of depth.
+
+        """
+        #Generate the appropriate dictionary keys
         sc = 'extra_'*level + 'sub_cats'
         ps = 'extra_'*(level+1) + 'pages'
 
+        #For each year, fetch the pages listed in the sub-category and the
+        #sub-sub-categories listed
         for year in tqdm(years):
             extra_pages = []
             extra_sub_cats = []
@@ -156,6 +262,7 @@ class Wiki:
             self.wiki_pages[year][ps] = extra_pages
             self.wiki_pages[year]['extra_'+sc] = extra_sub_cats
         
+        #List the years that have further sub-categories to fetch
         next_level_years = [year for year in years if self.wiki_pages[year]['extra_'+sc]]
         return next_level_years
 

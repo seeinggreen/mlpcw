6b0f7d5845ca90ec88c77c0c61efc80bfcebe3a3
hmehrafarin <houman.mehrafarin@gmail.com>
2023-03-12 18:32:28 +0000

    classification completed

--- classification.py
+++ classification.py
@@ -1,13 +1,13 @@
-from transformers import AutoModelForSequenceClassification, AutoTokenizer, TrainingArguments, Trainer
-import pandas as pd
+from transformers import AutoModelForSequenceClassification, AutoTokenizer, TrainingArguments, Trainer, TrainerCallback
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MultiLabelBinarizer
-import pyarrow as pa
-from datasets import Dataset, load_metric
+from results import *
+from datasets import Dataset
 from dataset import Dataset as dt
 from transformers import AutoModelForSequenceClassification
 import argparse
 from sklearn.preprocessing import MultiLabelBinarizer
+from sklearn.metrics import f1_score, hamming_loss, classification_report
 import numpy as np
 
 
@@ -17,16 +17,9 @@ EPOCHS = 3
 NUM_LABELS = 19
 
 
-def compute_metrics(eval_pred):
-    metric1 = load_metric("precision")
-    metric2 = load_metric("recall")
-    metric3 = load_metric("f1")
-    predictions, labels = eval_pred
-    predictions = np.argmax(predictions, axis=1)
-    precision = metric1.compute(predictions=predictions, references=labels)['precision']
-    recall = metric2.compute(predictions=predictions, references=labels)['recall']
-    f1 = metric3.compute(predictions=predictions, references=labels)['f1']
-    return {"precision": precision, "recall": recall, "f1": f1}
+class CustomCallBack(TrainerCallback):
+    def on_epoch_end(self, args, state, control, logs=None, **kwargs):
+        print(f"Epoch {state.epoch}: ")
 
 
 def load_data():
@@ -62,10 +55,27 @@ def process_data(row, plot):
     return encodings
 
 
+def compute_metrics(eval_pred):
+
+    predictions, labels = eval_pred
+    predictions = (predictions > 0).astype(int)
+    # Multi-label metrics:
+    # f1-macro --> overall f1 score for multi-label classification
+    # f1-micro --> per-class f1 score
+    f1_macro = f1_score(labels, predictions, average='macro')
+    f1_micro = f1_score(labels, predictions, average='micro')
+    hamming = hamming_loss(labels, predictions)
+
+    print(classification_report(labels, predictions, zero_division=0))
+
+    return {"f1_macro": f1_macro, "f1_micro": f1_micro, "hamming_loss": hamming}
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Predict movie genres using plots.")
-    parser.add_argument("--model", help="Select encoder model (BERT, RoBERTam DeBERTa, GPT-2)", choices=["bert", "roberta", "deberta", "gpt2"], default="bert")
+    parser.add_argument("--model", help="Select encoder model (BERT, RoBERTa, xlnet)", choices=["bert", "roberta", "xlnet"], default="bert")
     parser.add_argument("--plot_type", help="Select the plot you wish to do classification on", choices=['wiki', 'tmdb'])
+    parser.add_argument("--file_name", help="Choose an excel file name ending with .pkl")
     
     args = parser.parse_args()
 
@@ -83,6 +93,13 @@ if __name__ == "__main__":
         num_labels=NUM_LABELS,
         problem_type = "multi_label_classification"
         )
+    elif args.model == 'xlnet':
+        tokenizer = AutoTokenizer.from_pretrained('xlnet-base-cased')
+        model = AutoModelForSequenceClassification.from_pretrained(
+        'xlnet-base-cased',
+        num_labels=NUM_LABELS,
+        problem_type = "multi_label_classification"
+        )
 
     if args.plot_type == 'wiki':
         plot_type = 'wiki_plot'
@@ -95,7 +112,7 @@ if __name__ == "__main__":
 
     processed_data = []
 
-    for i in range(len(df)):
+    for i in range(1000):
         processed_data.append(process_data(df.iloc[i], plot_type))
 
     new_df = pd.DataFrame(processed_data)
@@ -106,8 +123,11 @@ if __name__ == "__main__":
         random_state=1234
     )
 
-    train_hg = Dataset(pa.Table.from_pandas(train_df))
-    valid_hg = Dataset(pa.Table.from_pandas(valid_df))
+    # train_hg = Dataset(pa.Table.from_pandas(train_df))
+    # valid_hg = Dataset(pa.Table.from_pandas(valid_df))
+    
+    train_hg = Dataset.from_pandas(train_df)
+    valid_hg = Dataset.from_pandas(valid_df)
 
     training_args = TrainingArguments(
         output_dir="./result", 
@@ -116,6 +136,11 @@ if __name__ == "__main__":
         per_device_eval_batch_size=BATCH_SIZE,
         learning_rate=LEARNING_RATE,
         num_train_epochs=EPOCHS,
+        save_strategy="epoch",
+        save_total_limit=2,
+        metric_for_best_model="f1_macro",
+        load_best_model_at_end=True,
+        weight_decay=0.01,
         )
 
     trainer = Trainer(
@@ -124,9 +149,12 @@ if __name__ == "__main__":
         train_dataset=train_hg,
         eval_dataset=valid_hg,
         tokenizer=tokenizer,
-        compute_metrics=compute_metrics
+        compute_metrics=compute_metrics,
+        callbacks = [CustomCallBack]
     )
 
     trainer.train()
 
-    trainer.evaluate()
+    model_results = trainer.evaluate()
+
+    write_results(model_results, args.file_name, args.model, args.plot_type)

ef5cd439a3fdfa17914dccb7a7baca6a205584e5
hmehrafarin <houman.mehrafarin@gmail.com>
2023-03-22 18:03:44 +0000

    comments added to results.py

--- results.py
+++ results.py
@@ -3,19 +3,19 @@ import os
 
 def write_confusion_matrix(file_name, conf_matrix):
     """
-    
+    Gets filename and confusion matrix returned by the model to create a table of TP, FP, TN, and FN 
+    for each class and write them in a pickle file.
 
     Parameters
     ----------
-    file_name : TYPE
-        DESCRIPTION.
-    conf_matrix : TYPE
-        DESCRIPTION.
+    file_name : str
+        File name specified by the user
+    conf_matrix : numpy.ndarray
+        A three dimensional array that includes the TP, NP, FP, FN for each class.
 
     Returns
     -------
-    dict : TYPE
-        DESCRIPTION.
+    None
 
     """
     
@@ -40,26 +40,27 @@ def write_confusion_matrix(file_name, conf_matrix):
 
         os.remove(file_path)
         df.to_pickle(file_path)
-    return dict
 
 def write_results(scores, file_name, model_name, plot_type, freeze, max_length):
     """
-    
+    Creates a table of F1 scores, Hamming Loss along with the model, the plot source it was trained on, 
+    whether it was trained, and the maximum token length in a table. It then stores the results in a pickle 
+    file.
 
     Parameters
     ----------
-    scores : TYPE
-        DESCRIPTION.
-    file_name : TYPE
-        DESCRIPTION.
-    model_name : TYPE
-        DESCRIPTION.
-    plot_type : TYPE
-        DESCRIPTION.
-    freeze : TYPE
-        DESCRIPTION.
-    max_length : TYPE
-        DESCRIPTION.
+    scores : dict
+        Consists of F1 Macro/Micro and Hamming Loss.
+    file_name : str
+        File name specified by the user.
+    model_name : str
+        The model used for encoding.
+    plot_type : str
+        The plot source used to carry out the training process.
+    freeze : bool
+        Whether the model was fine-tuned.
+    max_length : int
+        The maximum length of the input tokens.
 
     Returns
     -------
